0cacc9a73a76ac3a8a1a23fb05ce17052b6118b4
Aaron Gertler <aaronlgertler@gmail.com>
2018-05-25 23:52:40 -0700

    fleshed out 121 a bit more -- short session, at EA forum today

--- 121.py
+++ 121.py
@@ -15,5 +15,25 @@
 # After two turns, P(win) = 1/2 * 1/3 = 1/6
 # Three turns, P(win) = (1/2 * 1/3 * 1/4) + (1/2 * 1/3 * 1/4) RBB + (1/2 * 2/3 * 1/4) BRB + (1/2 * 1/3 * 3/4) BBR = 1 + 1 + 2 + 3 / 24 = 8/24 = 1/3
 # Four turns, P(win) = 1 + 1 + 2 + 3 + 4 / 5! = 11/120
-# Five turns, P(win) = P(5 blue) + P(four blue) + P(three blue)
+# Five turns, P(win) = P(five blue) + P(four blue) + P(three blue)
+# = 1 + (1+2+3+4+5) + (5 choose 2 = 10 arrangements of probabilities, which sum to...)
 
+# ...so what's the shorthand for what this sums to?
+# The denominator of our final fraction will always be (n+1)!, where n is the number of turns played.
+
+# The number of terms we add to find the numerator = 1 + n choose 1 + n choose 2... + n choose 7 (in the case of n = 15)
+# For each term, we multiply together all of the numerators of the "turns" where we find red discs
+# So if we pick 7 blue / 7 red, we have 15 choose 7 ways for that to happen, meaning 15 choose 7 "combinations" of numerators,
+# no repeats, between 1 and 15.
+
+# We can grab all of these by finding all permutations of 1-15, grabbing and sorting the first 7 terms of each, then keeping the unique "sets" of numbers
+# The same procedure applies to finding all the other terms for each probability.
+# Is there a shorter way to find the unique combinations of x terms, without having to find all the permutations of all 15 terms first?
+# Maybe, but since we only have to find ALL the permutations one time (for each number from 2-15), trying brute force first seems... fine?
+
+def permutations(list): # Return all permutations of a list of numbers, in list form
+
+
+
+
+# Remember to do lots of mapping here!
