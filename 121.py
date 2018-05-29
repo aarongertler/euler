6ff60b09c92db4af4d696365abd69fcf8430a0c4
Aaron Gertler <aaronlgertler@gmail.com>
2018-05-28 23:51:03 -0700

    finished 121

--- 121.py
+++ 121.py
@@ -13,7 +13,7 @@
 
 # After one turn, P(win) = 1/2
 # After two turns, P(win) = 1/2 * 1/3 = 1/6
-# Three turns, P(win) = (1/2 * 1/3 * 1/4) + (1/2 * 1/3 * 1/4) RBB + (1/2 * 2/3 * 1/4) BRB + (1/2 * 1/3 * 3/4) BBR = 1 + 1 + 2 + 3 / 24 = 8/24 = 1/3
+# Three turns, P(win) = (1/2 * 1/3 * 1/4) + (1/2 * 1/3 * 1/4) RBB + (1/2 * 2/3 * 1/4) BRB + (1/2 * 1/3 * 3/4) BBR = 1 + 1 + 2 + 3 / 24 = 7/24
 # Four turns, P(win) = 1 + 1 + 2 + 3 + 4 / 5! = 11/120
 # Five turns, P(win) = P(five blue) + P(four blue) + P(three blue)
 # = 1 + (1+2+3+4+5) + (5 choose 2 = 10 arrangements of probabilities, which sum to...)
@@ -23,20 +23,36 @@
 
 # The number of terms we add to find the numerator = 1 + n choose 1 + n choose 2... + n choose 7 (in the case of n = 15)
 # For each term, we multiply together all of the numerators of the "turns" where we find red discs
-# So if we pick 7 blue / 7 red, we have 15 choose 7 ways for that to happen, meaning 15 choose 7 "combinations" of numerators,
+# So if we pick 7 blues or 7 reds, we have 15 choose 7 ways for that to happen, meaning 15 choose 7 "combinations" of numerators,
 # no repeats, between 1 and 15.
 
 # We can grab all of these by finding all permutations of 1-15, grabbing and sorting the first 7 terms of each, then keeping the unique "sets" of numbers
 # The same procedure applies to finding all the other terms for each probability.
 # Is there a shorter way to find the unique combinations of x terms, without having to find all the permutations of all 15 terms first?
-# Maybe, but since we only have to find ALL the permutations one time (for each number from 2-15), trying brute force first seems... fine?
 
-def permutations(list): # Return all permutations of a list of numbers, in list form
+from itertools import combinations, chain # We don't want to double-count "red disk on turns 1 and 5" and "red disk on turns 5 and 1", so we won't be using permutations
+from math import factorial
+import numpy as np
 
 
+# print(list(combinations([1, 2, 3, 4], 2)))
+
+# for i in combinations([1, 2, 3, 4], 2):
+# 	print(i)
 
 
-# Remember to do lots of mapping here!
+def winning_odds(turns): # Odds of getting mostly blue disks after a certain number of turns
+	n_list = list(range(1, turns + 1))
+	denom = factorial(turns + 1)
+	num = 1 + sum(n_list) # Numerator of the fraction that shows us our odds
+	if turns % 2 == 1:
+		for i in range(2, int(turns / 2) + 1):
+			perms = list(combinations(n_list, i))
+			# print("Perms:", perms)
+			prods = np.array(list(map(np.prod, perms)), dtype='int64') # Fixed scalar overflow issue by moving to a more permissive numpy dtype: https://stackoverflow.com/questions/7559595/python-runtimewarning-overflow-encountered-in-long-scalars
+			# print("Prods:", prods)
+			num += sum(chain(prods))
+	print("Odds:", num, "/", denom)
+	print("Prize fund:", denom // num)
 
-# Commit for EA forum
-# Commit #2 for EA forum
+winning_odds(15) # Instantaneous, though Dreamshire's more "pure" method is certainly faster (my solution starts to get sluggish around 20 turns)
