dd7ac5bb8ab2d41e45b5e0d50fb7c628a959da5b
Aaron Gertler <aaronlgertler@gmail.com>
2018-06-10 23:42:59 -0700

    No progress today, EA Forum

--- 123.py
+++ 123.py
@@ -2,9 +2,12 @@
 
 # For example, when n = 3, p^3 = 5, and 4^3 + 6^3 = 280 ≡ 5 mod 25.
 
-# The least value of n for which the remainder first exceeds 10^9 is 7037.
+# The least value of n for which the remainder first exceeds 10^9 is 7037. 
 
 # Find the least value of n for which the remainder first exceeds 10^10.
 
 
+# Prime no. 7037 = 71059 (2 * n^2 is just over 10^10... hm)
 
+
+# (Leaving this open for today, didn't have any free time with Leaders' Forum)
