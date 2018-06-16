b44adf21c43ce5f57ab55a302c16c12962d45ed1
Aaron Gertler <aaronlgertler@gmail.com>
2018-06-15 23:46:47 -0700

    First version of 139 algorithm pre-edit

--- 139.py
+++ 139.py
@@ -19,7 +19,7 @@
 
 from math import sqrt, floor
 
-limit = 10**8
+limit = 10**3
 total = 0
 
 def square(n):
@@ -28,45 +28,27 @@ def square(n):
 def tile(bigger, smaller):
 	return square(bigger / smaller)
 
-
-
-# Insert pythagorean generator here (see 75.py)
+# Pythagorean generator from 75.py:
 
 for m in range(2, floor(sqrt(limit / 2))): # Double-check this limit if you aren't getting a working answer
 	for n in range (m-1, 0, -2):
-	long_side = m**2 + n**2
-	tri_area = m * n * (m**2 - n**2) # b = 2mn, a = m^2 - n^2, area = a*b / 2
-	big_area = long_side**2
-	small_area = big_area - (4 * tri_area)
+		long_side = m**2 + n**2
+		short_side = 2*m*n
+		med_side = m**2 - n**2
+		tri_area = m * n * (m**2 - n**2) # b = 2mn, a = m^2 - n^2, area = a*b / 2
+		big_area = long_side**2
+		small_area = big_area - (4 * tri_area)
+		print("Testing a square of", big_area, "with an inner square of", small_area)
 	if tile(big_area, small_area):
+		print("Triangle sides:", short_side, med_side, long_side, "Square area:", small_area)
 		total += 1
 
 print("Total:", total)
 
 
+# Getting weird results here. Can we add any more math to the tiling condition?
 
-	# All triples take the form a = m^2 - n^2, b = 2mn, c = m^2 + n^2
-# Meaning that a + b + c = 2*m^2 + 2mn
-# Can we generate numbers this way? m > n, so that a is a positive length
-# m = 2, n = 1, sum = 12, we have 3, 4, 5
-# m = 3, n = 1, sum = 24, (6, 8, 10) (not primitive)
-# m = 3, n = 2, sum = 30, (5, 12, 13)
-
-# Okay, so if we look at all m and n combinations where 2*m^2 + 2mn <= 1,500,000, that should work -- just exclude any sum that comes up twice
-# (Hopefully, this is fast enough...)
-# Upper limit of m at n = 1, 2*m^2 + 2m <= 1,500,000, so 2m(m + 1) <= 1,500,000, m(m + 1) <= 750,000, m <= 865
-
-from math import sqrt, floor
-from sympy import gcd
-
-limit = 1500000
-
-sums = [0]*(limit + 1) # Is using a list this large pratical? This may be the slowest part of the code right now (that can be adjusted to some other structure)
-
-for m in range(2, floor(sqrt(limit/2))): # See calculation above for setting range
-	for n in range(m - 1, 0, -2):
-		if gcd(m, n) == 1:  # We only want primitives, which requires that m and n be coprime
-			triangle_sum = 2*m*m + 2*m*n
-			for k in range(1, floor(limit / triangle_sum) + 1):
-				# placeholder = k*triangle_sum # Removing this seems to cut time by ~5%
-				sums[k * triangle_sum] += 1 
+# The area of the big square = a**2 + b**2 (since c = the root of that, but then we square c)
+# The area of the small square = (a - b)**2 (where a is the longer of the two short sides)
+# So we need to find that a**2 + b**2 can be tiled by a**2 - b**2 - 2ab
+# Editing the above equation to reflect this
