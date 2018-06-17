d92497a98e4da0d345a2578ff63957d71a75dc07
Aaron Gertler <aaronlgertler@gmail.com>
2018-06-16 17:03:18 -0700

    Finished 139

--- 139.py
+++ 139.py
@@ -19,36 +19,41 @@
 
 from math import sqrt, floor
 
-limit = 10**3
+limit = 10**8
 total = 0
 
 def square(n):
 	return sqrt(n) % 1 == 0
 
 def tile(bigger, smaller):
-	return square(bigger / smaller)
+	# return square(bigger / smaller)
+	return(sqrt(bigger) % sqrt(smaller) == 0) # Seems a bit faster
+
 
 # Pythagorean generator from 75.py:
 
-for m in range(2, floor(sqrt(limit / 2))): # Double-check this limit if you aren't getting a working answer
+# for m in range(2, floor(sqrt(limit / 2))): # Double-check this limit if you aren't getting a working answer
+for m in range(2, floor((sqrt(1 + 2*limit) / 2) - 1)):
 	for n in range (m-1, 0, -2):
-		long_side = m**2 + n**2
-		short_side = 2*m*n
-		med_side = m**2 - n**2
-		tri_area = m * n * (m**2 - n**2) # b = 2mn, a = m^2 - n^2, area = a*b / 2
-		big_area = long_side**2
-		small_area = big_area - (4 * tri_area)
-		print("Testing a square of", big_area, "with an inner square of", small_area)
-	if tile(big_area, small_area):
-		print("Triangle sides:", short_side, med_side, long_side, "Square area:", small_area)
-		total += 1
+		a = 2*m*n
+		b = m**2 - n**2
+		perimeter = (2 * m**2) + 2*m*n
+		big_area = a**2 + b**2
+		small_area = (a - b)**2
+		# if m == 12:
+			# print("Testing n = ", n)
+			# print("Big and small:", big_area, small_area)
+			# print("Testing a square of", big_area, "with an inner square of", small_area)
+		# if tile(big_area, small_area): # Just an indentation error the whole time... was only finding one working number for each m
+			# print("Triangle sides:", big_area, "Square area:", small_area, "m and n =", m, n)
+		if small_area == 1: # The "tile" function double-counts certain working numbers, since everything that tiles with a number greater than 1 is (I think?) derived from a primitive that tiles with 1
+			total += limit // perimeter # All non-primitives derived from a working primitive will also work, and we can multiply our sides until the perimeter is just under the limit
 
 print("Total:", total)
 
-
-# Getting weird results here. Can we add any more math to the tiling condition?
 
 # The area of the big square = a**2 + b**2 (since c = the root of that, but then we square c)
 # The area of the small square = (a - b)**2 (where a is the longer of the two short sides)
 # So we need to find that a**2 + b**2 can be tiled by a**2 - b**2 - 2ab
 # Editing the above equation to reflect this
+
